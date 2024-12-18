462e510ad172d37dde986f2e21a8e9a86619353b
‫Elnatan Lazar <elnatan100@gmail.com>
2024-12-18 11:20:18 +0200

    Update README.py

--- README.py
+++ README.py
@@ -1,23 +1,27 @@
-PS C:\Users\xgdbtslb\sweyntooth_bluetooth_low_energy_attacks> pytest -vv pyTest.py --junitxml=./test.xml --html=pytest_report.html --self-contained-html
-
-
 import subprocess
 import pytest
 from threading import Timer
+import os
 
 # Default parameters
 TIME_FOR_TEST = 3  # Timeout for each script in seconds
 COM_PORT = "COM4"
 MAC_ADDRESS = "84:72:93:3c:5d:d6"
 
-# List of attack scripts to run
-attack_scripts = [
-    "./CC2640R2_public_key_crash.py",
-    "./CC_connection_req_crash.py"
-]
+# Path to the directory containing attack scripts
+SCRIPTS_DIRECTORY = "./attack_scripts"
 
 # The failure phrase to look for in the output
 FAILURE_PHRASE = "The device may have crashed"
+
+# Dynamically list all Python files in the specified directory
+def get_attack_scripts(directory):
+    return [
+        os.path.join(directory, f) for f in os.listdir(directory)
+        if f.endswith(".py")
+    ]
+
+attack_scripts = get_attack_scripts(SCRIPTS_DIRECTORY)
 
 @pytest.mark.parametrize("script_name", attack_scripts)
 def test_attack_script(script_name):
@@ -39,7 +43,6 @@ def test_attack_script(script_name):
     # Combine stdout and stderr for full output
     full_output = stdout + stderr
     print(f"Output of {script_name}:\n{full_output}\n{'=' * 50}")
-
 
     # Assert that the output does not contain the failure phrase
     assert FAILURE_PHRASE not in full_output, f"Script {script_name} failed. Found '{FAILURE_PHRASE}' in output."
